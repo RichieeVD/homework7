a8151ff17430c6a6bdc4748ba9c1354d96d927c9
rvd <nseries_nokia@list.ru>
2026-02-24 19:11:00 +0400

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Задание 1
+        int firstFriday = 3; // Предположим, первая пятница выпала на 3-е число
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int currentDay = firstFriday; currentDay <= 31; currentDay += 7) {
+            System.out.println("Сегодня пятница, " + currentDay + "-е число. Необходимо подготовить отчет");
+
         }
     }
 }
